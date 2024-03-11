5656376c8db4c1e8ed72ace32f38f79a6496d472
Maheswar Mahapatra <maheswar2110@gmail.com>
2024-03-11 20:08:13 +0530

    apos fixed

--- src/components/testnearcomponents/schedule.tsx
+++ src/components/testnearcomponents/schedule.tsx
@@ -10,7 +10,7 @@ export default function schedule() {
       <div>
         <span>Make a Schedule</span>
         <div>Make a schedule in advance with the available doctor</div>
-        <p>Lab test is a very painful process, especially if you{"'"}re not taking care of your health and having regular check-ups. Vaid makes it easier for everyone to schedule a doctor's appointment.</p>
+        <p>Lab test is a very painful process, especially if you{"'"}re not taking care of your health and having regular check-ups. Vaid makes it easier for everyone to schedule a doctor{"'"}s appointment.</p>
         <div>
             <span>Make a schedule online is easy</span>
             <span>Easy to connect with nearest lab</span>
